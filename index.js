08d0548db9b43c7e1169c4216b5559bf272a38f3
RachaelMuriithi <rachael.mureithi@student.moringaschool.com>
2022-08-15 23:38:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Code your solution in this file!
+const companyName = 'Scuber';
+// const companyName = 'Scuber'
+// const mostProfitableNeighnorhood = 'Chelsea'
+let mostProfitableNeighborhood = 'Chelsea';
+let companyCeo = 'Susan Smith';
